a5527cdba8fe10b6af9d12a33bf7f0abbb119bf6
DelroyGayle <delroygayle01@gmail.com>
2023-09-27 15:49:14 +0000

    CI version of 'testing_with_python_02' In the previous commit I meant 02. NOTE: the typo was in line 7 of evens.py

--- evens.py
+++ evens.py
@@ -11,15 +11,17 @@ def even_number_of_evens(numbers):
             return False
         else:
             evens = 0
-
+            
         for n in numbers:
             if n % 2 == 0:
                 evens += 1
-
-        return evens and evens % 2 == 0
-
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
     else:
         raise TypeError("A list was not passed into the function")
+    return None
 
 if __name__ == "__main__":
-    print(even_number_of_evens([2, 1, 4]))
+    print(even_number_of_evens(5))
